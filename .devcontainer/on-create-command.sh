10b5aa92aa50db6ce0b07f4871b6f2d856c1171b
Jake Wilkins <jakewilkins@github.com>
2021-08-19 03:48:25 +0000

    install apptokit and setup local .apptokit.yml file

--- .devcontainer/on-create-command.sh
+++ .devcontainer/on-create-command.sh
@@ -1,3 +1,20 @@
 #!/bin/bash
 
-echo word dog
+playgroud_dir=$(pwd)
+
+hash apptokit 2>/dev/null || {
+  cd /workspaces
+
+  git clone https://github.com/jakewilkins/apptokit
+
+  cd apptokit
+
+  bash install.sh install --i-have-read-this-script
+
+  cd $playground_dir
+}
+
+app_name="Playground App $HOSTNAME"
+
+sed -i "s/%APP_NAME%/$app_name/g" ./.apptokit.yml
+sed -i "s/%CODESPACES_NAME%/$CODESPACE_NAME/g" ./.apptokit.yml
